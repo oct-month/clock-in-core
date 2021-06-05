f479348f692d3f4325d2cd3aa7c32f74e3039ad7
ablocker <oct-month@qq.com>
2021-06-05 21:48:51 +0800

    The First Commit

---
+++ src/test/java/snnu/cs/clock/ClockApplicationTests.java
@@ -0,0 +1,13 @@
+package snnu.cs.clock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
